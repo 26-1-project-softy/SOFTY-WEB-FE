c3f1ddfbb1c4c211112b08bdba6ae766ec4abf42
cjiyun <chlwldbs78@gmail.com>
2026-04-23 21:33:54 +0900

    style: 전역 스타일 보완, 색상 및 폰트 토큰 수정

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -15,15 +15,15 @@ const createFontStyleNew = (family: string, weight: number, size: number, lineHe
 `;
 
 export const fonts = {
-  title1: createFontStyle('Pretendard', 900, 36, 150),
-  title2: createFontStyle('Pretendard', 600, 32, 150),
-  title3: createFontStyle('Pretendard', 600, 24, 150),
-  title4: createFontStyle('Pretendard', 600, 18, 150),
-  labelL: createFontStyleNew('Pretendard', 700, 24, 30),
-  labelM: createFontStyleNew('Pretendard', 700, 20, 24),
+  title1: createFontStyle('Pretendard', 900, 28, 150),
+  title2: createFontStyle('Pretendard', 600, 24, 150),
+  title3: createFontStyle('Pretendard', 600, 18, 150),
+  title4: createFontStyle('Pretendard', 600, 16, 150),
+  labelL: createFontStyleNew('Pretendard', 700, 24, 36),
+  labelM: createFontStyleNew('Pretendard', 700, 20, 30),
   labelS: createFontStyleNew('Pretendard', 600, 16, 19),
   labelXS: createFontStyleNew('Pretendard', 600, 15, 18),
-  body1: createFontStyle('Pretendard', 400, 20, 150),
+  body1: createFontStyle('Pretendard', 400, 16, 150),
   body2: createFontStyle('Pretendard', 400, 15, 150),
   body3: createFontStyle('Pretendard', 400, 14, 150),
   caption: createFontStyle('Pretendard', 400, 12, 150),
