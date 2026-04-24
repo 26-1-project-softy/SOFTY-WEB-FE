e5c9e07985cbe462fa1f8f1bbdc04f009cb92282
seoiy <in02eh5@gmail.com>
2026-04-24 21:55:04 +0900

    chore: 개발용 로그인 우회 코드 제거

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,24 +1,15 @@
-import { useEffect, type PropsWithChildren } from 'react';
+﻿import { useEffect, type PropsWithChildren } from 'react';
 import { Loader } from '@/components/common/Loader';
 import { useAuthStore } from '@/stores/authStore';
-import { applyDevLogin } from '@/services/auth/devAuth';
 
 export const AuthProvider = ({ children }: PropsWithChildren) => {
   const isAuthInitialized = useAuthStore(state => state.isAuthInitialized);
-  const setAuth = useAuthStore(state => state.setAuth);
   const setAuthInitialized = useAuthStore(state => state.setAuthInitialized);
 
   useEffect(() => {
-    const didApplyDevLogin = applyDevLogin({ setAuth, setAuthInitialized });
-
-    if (didApplyDevLogin) {
-      return;
-    }
-
-    // TODO: 로그인/로그아웃/현재 사용자 조회 로직은 인증 구현 단계에서 연결
-    // auth 초기화 완료 처리
+    // TODO: 로그인/현재 사용자 조회 로직은 인증 연동 단계에서 연결
     setAuthInitialized(true);
-  }, [setAuth, setAuthInitialized]);
+  }, [setAuthInitialized]);
 
   if (!isAuthInitialized) {
     return <Loader />;
